881e6b10b50e5ab0c0f5e4bdc4e2ec30788c8d32
Robin Berjon <robin.berjon@nytimes.com>
2025-06-23 19:21:01 +0200

    start converting

--- examples/basic/nemik.config.js
+++ examples/basic/nemik.config.js
@@ -3,7 +3,8 @@
 export default async function (nemik) {
   nemik
     .warn(`This seems to work.`)
-    .gdoc('gdoc-rb', '1NM8_Ww-Q6ER4hLgyrAtTrGINQouc1j-CleeDiCTA6A4')
-    .data('gdoc', (data) => console.warn(JSON.stringify(data, null, 2)))
+    .gdoc('gdoc-rb', '1NM8_Ww-Q6ER4hLgyrAtTrGINQouc1j-CleeDiCTA6A4', { save: 'book2.json' })
+    // .data('gdoc', (data) => console.warn(JSON.stringify(data, null, 2)))
+    .gdoc2html({ save: 'book2.html' })
   ;
 }
