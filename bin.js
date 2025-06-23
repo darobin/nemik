b1bf229c0bc6cc6f2f26d12d5c4c7c79ad7ec989
Robin Berjon <robin.berjon@nytimes.com>
2025-06-23 12:07:55 +0200

    get started

---
+++ bin.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+import { cwd, exit } from "node:process";
+import { isAbsolute, join, extname } from "node:path";
+import { createReadStream, createWriteStream } from "node:fs";
+import { readFile } from "node:fs/promises";
+import { program } from 'commander';
+import { addCredentials } from "./lib/credentials.js";
+import makeRel from './lib/rel.js';
+
+const rel = makeRel(import.meta.url);
+const { version } = JSON.parse(await readFile(rel('./package.json')));
+
+program
+  .name('nemik')
+  .description('All-purpose document generation')
+  .version(version)
+;
+
+// credentials management
+const credentials = program.command('credentials');
+credentials
+  .command('set')
+  .argument('<service>', 'the key for which these credentials are for')
+  .argument('<account>')
+  .argument('<password>')
+  .action(async (service, account, password) => {
+    await addCredentials(service, account, password);
+  })
+;
+
+// watch
+
+
+// run
+
+
+program.parse();
+
+function absolutise (path) {
+  return isAbsolute(path) ? path : join(cwd(), path);
+}
+
+function die (str) {
+  console.error(`Error: ${str}`);
+  exit(1);
+}
