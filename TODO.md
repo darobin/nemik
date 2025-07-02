68ef85050b2ecf312f43a3a90619b43f4dd81c76
Robin Berjon <robin.berjon@nytimes.com>
2025-07-02 11:31:47 +0200

    better theming

--- TODO.md
+++ TODO.md
@@ -2,5 +2,6 @@
 # Next
 - [ ] Themes
 - [ ] Sectionify
+- [ ] Add weasyprint and generate PDF
 - [ ] Do images differently because the URL changes unreliably
       (post process and use a CID instead + media type)
--- themes/supramundane.js
+++ themes/supramundane.js
@@ -4,6 +4,7 @@ import makeRel from '../lib/rel.js';
 
 const rel = makeRel(import.meta.url);
 const files = [
+  'Catamaran-VariableFont_wght.ttf',
   'JosefinSans-Italic-VariableFont_wght.ttf',
   'JosefinSans-VariableFont_wght.ttf',
   'Mulish-Italic-VariableFont_wght.ttf',
@@ -14,11 +15,32 @@ const files = [
 
 export default async function (opts, ctx, data) {
   const { el } = htmlHelpers(data.document);
+  const { head, body } = data.document;
   await Promise.all(
     files.map(f => {
       const to = `.nemik/${/svg/.test(f) ? 'img' : 'css'}/${f}`;
       return ctx.cpToRel(rel(`supramundane/${f}`), to);
     })
   );
-  el('link', { rel: 'stylesheet', href: '.nemik/css/supramundane.css' }, [], data.document.head);
+  // XXX a lot of the below should be shared between themes
+  el('link', { rel: 'stylesheet', href: '.nemik/css/supramundane.css' }, [], head);
+  // detabbify
+  let tab = body.querySelector('.tab');
+  while (tab) {
+    const df = data.document.createDocumentFragment();
+    while (tab.hasChildNodes()) df.append(tab.firstChild);
+    tab.replaceWith(df);
+    tab = body.querySelector('.tab')
+  }
+  // mainify
+  const main = el('main', {}, []);
+  const header = body.querySelector('header');
+  while (header.nextSibling) main.append(header.nextSibling);
+  body.append(main);
+  // clean
+  [...body.querySelectorAll('.section-break-continuous, p:empty')].forEach(e => e.remove());
+  // XXX
+  // - sections
+  // - everything before section is the abstract
+  // - toc
 }
